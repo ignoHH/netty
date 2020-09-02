327006af9ec2e0190160414044a9d9810807245b
ignoHH <ignohh@outlook.com>
2020-09-02 09:45:24 +0800

    initial push

--- src/main/java/com/example/netty/handler/DiscardServerHandler.java
+++ src/main/java/com/example/netty/handler/DiscardServerHandler.java
@@ -1,5 +1,23 @@
-package com.example.netty.handler;/**
- * @author wangyu
+package com.example.netty.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author ignohh
  * @date 2020-09-02 9:33
- */public class DiscardServerHandler {
+ * 处理服务端
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ((ByteBuf) msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
