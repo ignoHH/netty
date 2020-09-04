06a5eb0e8a85e4cdad25f21a3109b7720ff9355c
ignoHH <ignohh@outlook.com>
2020-09-04 17:19:33 +0800

    solution1

---
+++ nettysolution1/handler/DiscardServerHandler.java
@@ -0,0 +1,46 @@
+package com.example.netty.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * 处理服务端
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        /*ByteBuf in = (ByteBuf)msg;
+        try {
+            *//*while (in.isReadable()) {
+                System.out.print((char)in.readByte());
+                System.out.flush();
+            }*//*
+            *//*System.out.println(in.toString(io.netty.util.CharsetUtil.US_ASCII));*//*
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }*/
+
+        // write(Object) 方法来逐字地把接受到的消息写入。
+        // 请注意不同于 DISCARD 的例子我们并没有释放接受到的消息，
+        // 这是因为当写入的时候 Netty 已经帮我们释放了
+        ctx.write(msg);
+        // ctx.write(Object) 方法不会使消息写入到通道上，他被缓冲在了内部，
+        // 你需要调用 ctx.flush() 方法来把缓冲区中数据强行输出。
+        // 或者你可以用更简洁的 cxt.writeAndFlush(msg) 以达到同样的目的
+        ctx.flush();
+
+    }
+    /*@Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ((ByteBuf) msg).release();
+    }*/
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ nettysolution1/handler/TimeClientHandler.java
@@ -0,0 +1,53 @@
+package com.example.netty.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.Date;
+
+public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+    private ByteBuf buf;
+
+    // ChannelHandler 有2个生命周期的监听方法：handlerAdded()和 handlerRemoved()。
+    // 你可以完成任意初始化任务只要他不会被阻塞很长的时间
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) {
+        buf = ctx.alloc().buffer(4);
+    }
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) {
+        buf.release();
+        buf = null;
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        //在TCP/IP中，Netty 会把读到的数据放到 ByteBuf 的数据结构中
+        ByteBuf m = (ByteBuf) msg;
+        /*try {
+            long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
+            System.out.println(new Date(currentTimeMillis));
+            ctx.close();
+        } finally {
+            m.release();
+        }*/
+        //首先，所有接收的数据都应该被累积在 buf 变量里
+        buf.writeBytes(m);
+        m.release();
+
+        // 然后，处理器必须检查 buf 变量是否有足够的数据，在这个例子中是4个字节，
+        // 然后处理实际的业务逻辑。否则，Netty 会重复调用channelRead() 当有更多数据到达直到4个字节的数据被积累
+        if (buf.readableBytes() >= 4) {
+            long currentTimeMillis = (buf.readUnsignedInt() - 2208988800L) * 1000L;
+            System.out.println(new Date(currentTimeMillis));
+            ctx.close();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
