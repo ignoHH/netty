3130349dd4fa81228311137a39709ec6c69c58cc
ignohh <ignohh@outlook.com>
2020-09-05 22:46:17 +0800

    solution2

--- src/main/java/com/example/netty/server/TimeServer.java
+++ src/main/java/com/example/netty/server/TimeServer.java
@@ -31,7 +31,7 @@ public class TimeServer {
                     .channel(NioServerSocketChannel.class)
                     // 这里的事件处理类经常会被用来处理一个最近的已经接收的 Channel。
                     // ChannelInitializer 是一个特殊的处理类，他的目的是帮助使用者配置一个新的 Channel。
-                    // 也许你想通过增加一些处理类比如DiscardServerHandler 来配置一个新的 Channel
+                    // 也许你想通过增加一些处理类比如TimeServerHandler 来配置一个新的 Channel
                     // 或者其对应的ChannelPipeline 来实现你的网络程序。
                     // 当你的程序变的复杂时，可能你会增加更多的处理类到 pipline 上，然后提取这些匿名类到最顶层的类上
                     .childHandler(new ChannelInitializer<SocketChannel>() {
@@ -45,13 +45,12 @@ public class TimeServer {
                     // 请参考 ChannelOption 和详细的 ChannelConfig 实现的接口文档
                     // 以此可以对ChannelOption 的有一个大概的认识
                     .option(ChannelOption.SO_BACKLOG, 128)
-                    // 你关注过 option() 和 childOption() 吗？
                     // option() 是提供给NioServerSocketChannel 用来接收进来的连接。
                     // childOption() 是提供给由父管道 ServerChannel 接收到的连接，
                     // 在这个例子中也是 NioServerSocketChannel
                     .childOption(ChannelOption.SO_KEEPALIVE, true);
 
-            // 我们继续，剩下的就是绑定端口然后启动服务。这里我们在机器上绑定了机器所有网卡上的 8080 端口。
+            // 我们继续，剩下的就是绑定端口然后启动服务。
             // 当然现在你可以多次调用 bind() 方法(基于不同绑定地址)
             ChannelFuture f = b.bind(port).sync();
 
